f31119f0a9c3f9e67bee0f14c4ef1fdead98099a
charlie z <charlie.j.zhang@pwc.com>
2020-07-15 15:03:43 +0800

    add logging

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,11 @@ from flask_migrate import Migrate
 import sqlalchemy
 from sqlalchemy.ext.declarative import api
 
+# logging
+import logging
+from logging.handlers import RotatingFileHandler
+import os
+
 from config import Config, DevelopmentConfig, ProductionConfig
 
 db = SQLAlchemy()
@@ -25,6 +30,26 @@ def create_app(config=DevelopmentConfig):
 
     from app.auth import bp as auth_bp
     app.register_blueprint(auth_bp, url_prefix='auth')
+
+    if not app.debug:
+        if not os.path.exists('logs'):
+            os.mkdir('logs')
+        '''The `RotatingFileHandler` class is nice because it rotates the logs,
+            ensuring that the log files do not grow too large
+            when the application runs for a long time. 
+        '''
+        file_handler = RotatingFileHandler('logs/application.log', maxBytes=10240,
+                                       backupCount=10)
+        file_handler.setFormatter(logging.Formatter(
+            '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+        '''logging categories, they are DEBUG, INFO, WARNING, ERROR and CRITICAL
+            in increasing order of severity.
+        '''
+        file_handler.setLevel(logging.INFO)
+        app.logger.addHandler(file_handler)
+
+        app.logger.setLevel(logging.INFO)
+        app.logger.info('Application startup')
 
     return app
 
