990c479b5a4c931975996ef200046a28c5c1b7a3
charlie z <charlie.j.zhang@pwc.com>
2020-07-15 15:28:17 +0800

    Model: User: add to_dict function

--- app/models.py
+++ app/models.py
@@ -15,6 +15,16 @@ class User(db.Model):
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
 
+    
+    def to_dict(self):
+        data = {
+            'id': self.id,
+            'username': self.username,
+
+        }
+
+        return data
+
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
