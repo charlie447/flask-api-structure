564f00edf977c5c98decb971ec6612675714d2a5
charlie z <charlie.j.zhang@pwc.com>
2020-07-15 15:11:37 +0800

    Update user api but with empty functions

--- app/api/user.py
+++ app/api/user.py
@@ -1,1 +1,35 @@
 from app.api import bp
+
+
+@bp.route('/users/<int:id>', methods=['GET'])
+def get_user(id):
+    """Return a user.
+
+    Args:
+        id (int): the user id.
+    """
+    pass
+
+
+@bp.route('/users', methods=['GET'])
+def get_users():
+    """Return the collection of all users.
+    """
+    pass
+
+
+@bp.route('/users', methods=['POST'])
+def create_user():
+    """	Register a new user account.
+    """
+    pass
+
+
+@bp.route('/users/<int:id>', methods=['PUT'])
+def update_user(id):
+    """	Modify a user.
+
+    Args:
+        id (int): User ID.
+    """
+    pass
