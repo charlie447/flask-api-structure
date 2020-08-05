1915694cd637d7fbec581d9f9e12a0e14d67dc18
charlie z <charlie.j.zhang@pwc.com>
2020-08-06 00:17:30 +0800

    Tasks: add internal function for setting task progress(Not done, needs to implement notification)

--- app/tasks.py
+++ app/tasks.py
@@ -1,7 +1,25 @@
 from app import create_app
 
+from rq import get_current_job
+from app import db
+from app.models import Task
+
 # needs to create its own application instance if the task functions need it.
 app = create_app()
 # pushing a context makes the application be the "current" application instance
 app.app_context().push()
 
+
+def _set_task_progress(progress):
+    job = get_current_job()
+    if job:
+        job.meta['progress'] = progress
+        job.save_meta()
+        # get task from db
+        task = Task.query.get(job.get_id())
+        # push notifications to the client
+        # task.user.add_notification('task_progress', {'task_id': job.get_id(),
+        #                                              'progress': progress})
+        if progress >= 100:
+            task.complete = True
+        db.session.commit()
